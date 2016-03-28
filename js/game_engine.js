540f42415caaa5dae6035280078193625a8791a3
Michael Henretty <michael.henretty@gmail.com>
2016-03-28 23:11:57 +0200

    cleanup game win logic

--- js/game_engine.js
+++ js/game_engine.js
@@ -13,7 +13,6 @@
       new Player('Player 3')
     ];
 
-    this.gameover = false;
     this.frame = 0;
     this.lane = new Lane();
     this.lane.onroll = this.roll.bind(this);
@@ -21,55 +20,65 @@
   }
 
   GameEngine.prototype.start = function() {
-    this.gameover = false;
     this.frame = 1;
     this.currentPlayer = 0;
     this.render();
     this.update();
   };
 
-  GameEngine.prototype.checkWin = function() {
-    if (this.frame > MAX_FRAMES) {
-      this.gameover = true;
+  GameEngine.prototype.isGameOver = function() {
+    return this.frame > MAX_FRAMES;
+  };
 
-      // Get player with highest score.
-      var winningPlayer = 0;
-      var highScore = this.players[winningPlayer].score;
-      var isTie = false;
-      for (var i = 1; i < this.players.length; i++) {
-        if (this.players[i].score === highScore) {
-          isTie = true;
-        } else {
-          isTie = false;
-          if (this.players[i].score > highScore) {
-            winningPlayer = i;
-            highScore = this.players[i].score;
-          }
+  GameEngine.prototype.getWinner = function() {
+    // Get player with highest score.
+    var winningPlayer = 0;
+    var highScore = this.players[winningPlayer].score;
+    var isTie = false;
+    for (var i = 1; i < this.players.length; i++) {
+      if (this.players[i].score === highScore) {
+        isTie = true;
+      } else {
+        isTie = false;
+        if (this.players[i].score > highScore) {
+          winningPlayer = i;
+          highScore = this.players[i].score;
         }
       }
-
-      // Now display who won.
-      var winContainer = document.createElement('p');
-      if (isTie) {
-        winContainer.textContent = 'It\'s a tie :/';
-      } else {
-        winContainer.textContent = this.players[winningPlayer].name + ' wins!';
-      }
-      this.lane.container.remove();
-      this.container.appendChild(winContainer);
-
-      // Allow the game to be reset.
-      var resetButton = document.createElement('button');
-      resetButton.textContent = 'Reset';
-      resetButton.onclick = function() {
-        this.onreset && this.onreset();
-      }.bind(this);
-      this.container.appendChild(resetButton);
     }
+    // Return -1 if game is a tie, player index otherwise.
+    return isTie ? -1 : winningPlayer;
+  };
+
+  GameEngine.prototype.displayResetButton = function() {
+    var resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.onclick = function() {
+      this.onreset && this.onreset();
+    }.bind(this);
+    this.container.appendChild(resetButton);
+  };
+
+  GameEngine.prototype.displayWin = function() {
+    if (!this.isGameOver()) {
+      console.error('Cannot display win screen, game still on');
+    }
+
+    // Now display who won.
+    var winner = this.getWinner();
+    var winContainer = document.createElement('p');
+    if (winner === -1) {
+      winContainer.textContent = 'It\'s a tie :/';
+    } else {
+      winContainer.textContent = this.players[winningPlayer].name + ' wins!';
+    }
+    this.lane.container.remove();
+    this.container.appendChild(winContainer);
+    this.displayResetButton();
   };
 
   GameEngine.prototype.roll = function(pins) {
-    if (this.gameover) {
+    if (this.isGameOver()) {
       // Do nothing if game is over.
       return;
     }
@@ -82,7 +91,9 @@
     }
     this.update();
 
-    this.checkWin();
+    if (this.isGameOver()) {
+      this.displayWin();
+    }
   };
 
   GameEngine.prototype.render = function() {
@@ -100,7 +111,8 @@
   };
 
   GameEngine.prototype.update = function() {
-    this.frameContainer.textContent = 'Frame ' + this.frame;
+    this.frameContainer.textContent = this.isGameOver() ?
+      'Game Over' :'Frame ' + this.frame;
     this.scorecard.update();
   };
 
