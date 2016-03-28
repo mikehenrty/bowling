103a1518c6d1de49e407ae772a0c9741887f31ad
Michael Henretty <michael.henretty@gmail.com>
2016-03-28 21:25:27 +0200

    add logic to see who wins at the end

--- js/game_engine.js
+++ js/game_engine.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  const MAX_FRAMES = 10;
+
   /**
    * Game Engine
    * Orchestrates input, scoring and game display.
@@ -9,9 +11,11 @@
   function GameEngine() {
     this.players = [
       new Player('Player 1'),
+      new Player('Player 1.3'),
       new Player('Player 2')
     ];
 
+    this.gameover = false;
     this.frame = 0;
     this.lane = new Lane();
     this.lane.onroll = this.roll.bind(this);
@@ -20,12 +24,50 @@
   }
 
   GameEngine.prototype.start = function() {
+    this.gameover = false;
     this.frame = 1;
     this.currentPlayer = 0;
     this.render();
   };
 
+  GameEngine.prototype.checkWin = function() {
+    if (this.frame > MAX_FRAMES) {
+      this.gameover = true;
+
+      // Get player with highest score.
+      var winningPlayer = 0;
+      var highScore = this.players[winningPlayer].score;
+      var isTie = false;
+      for (var i = 1; i < this.players.length; i++) {
+        if (this.players[i].score === highScore) {
+          isTie = true;
+        } else {
+          isTie = false;
+          if (this.players[i].score > highScore) {
+            winningPlayer = i;
+            highScore = this.players[i].score;
+          }
+        }
+      }
+
+      // Now display who won.
+      var winContainer = document.createElement('p');
+      if (isTie) {
+        winContainer.textContent = 'It\'s a tie :/';
+      } else {
+        winContainer.textContent = this.players[winningPlayer].name + ' wins!';
+      }
+      this.lane.container.remove();
+      this.container.appendChild(winContainer);
+    }
+  };
+
   GameEngine.prototype.roll = function(pins) {
+    if (this.gameover) {
+      // Do nothing if game is over.
+      return;
+    }
+
     this.players[this.currentPlayer].addPoints(pins);
     this.currentPlayer++;
     if (this.currentPlayer >= this.players.length) {
@@ -33,6 +75,8 @@
       this.frame++;
     }
     this.render();
+
+    this.checkWin();
   };
 
   GameEngine.prototype.render = function() {
